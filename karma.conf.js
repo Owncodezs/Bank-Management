47571bb76608625bf33e4b570be19f784218fc0f
manibharathi <manibharathidct@gmai.com>
2023-01-22 11:52:39 +0530

    clint docker image

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/gate-pass-new'),
+      dir: require('path').join(__dirname, './coverage/mainframe'),
       subdir: '.',
       reporters: [
         { type: 'html' },
